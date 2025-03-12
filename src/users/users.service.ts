fc7df109ddd57309fe16f730bd38a2e38bd49f39
Krzysztof Wilczyński <krzysztof.wilczynski@aplitt.pl>
2025-03-12 11:02:49 +0100

    update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -20,17 +20,24 @@ export class UsersService {
     return this.repo.find({ where: { email } });
   }
 
-  async update(id: number, attrs: Partial<User> ) {
+  async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
 
-    if(!user) {
-      throw new Error('user not found')
+    if (!user) {
+      throw new NotFoundException('user not found');
     }
-    Object.assign(user, attrs)
-    return this.repo.save(user)
+    Object.assign(user, attrs);
+    return this.repo.save(user);
   }
 
-  remove() {}
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return this.repo.remove(user);
+  }
 }
 
 // TODO: Shift+ENTER w insercie do pisania w nowej linii
@@ -38,3 +45,6 @@ export class UsersService {
 // Proofchecking języków naturalnych
 // Snippety (~log)
 // C-q do wyświetlania dokumentacji
+// Chcę mieć lualine na szerokości całego ekranu, nie okna
+// Wkurwia mnie deferrowanie podpowiedzi
+//
